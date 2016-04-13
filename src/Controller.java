1559bbe6b5931a246b10754e02d2a1beff9f7fcd
Kyrill van Seventer <k.vanseventer@gmail.com>
2016-04-13 14:24:32 +0200

    Graph aangepast

--- src/Controller.java
+++ src/Controller.java
@@ -8,15 +8,17 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
 public class Controller implements Initializable {
 
     @FXML
-    public LineChart chart;
+    public BarChart chart;
 
     @FXML
     public TextField Plaats;
@@ -25,14 +27,14 @@ public class Controller implements Initializable {
 
 
     @FXML
-    public LineChart<String, Number> lineChart;
+    public BarChart<String, Number> barChart;
 
     public void btn(ActionEvent event) {
         XYChart.Series<String, Number> series = new XYChart.Series<String, Number>();
         series.getData().add(new XYChart.Data<String, Number>("Jan", 200));
         series.getData().add(new XYChart.Data<String, Number>("Feb", 600));
         series.getData().add(new XYChart.Data<String, Number>("Mar", 400));
-        lineChart.getData().add(series);
+        barChart.getData().add(series);
     }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
