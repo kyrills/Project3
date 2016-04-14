b47b360ba457dcc1cb2518b30343550862f557b9
Vitrav <dave.visser@outlook.com>
2016-04-14 11:11:53 +0200

    Charts geupdated voorlopig

--- src/Controller.java
+++ src/Controller.java
@@ -8,10 +8,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
+import javafx.scene.chart.*;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -24,10 +21,17 @@ public class Controller implements Initializable {
     public TextField Plaats;
 
 
-
-
     @FXML
     public BarChart<String, Number> barChart;
+
+    @FXML
+    public BarChart<String, Number> barChart2;
+
+    @FXML
+    public LineChart<String, Number> lineChart;
+
+    @FXML
+    public PieChart pieChart;
 
     public void btn(ActionEvent event) {
         XYChart.Series<String, Number> series = new XYChart.Series<String, Number>();
@@ -36,6 +40,42 @@ public class Controller implements Initializable {
         series.getData().add(new XYChart.Data<String, Number>("Mar", 400));
         barChart.getData().add(series);
     }
+
+    public void btn3(ActionEvent event){
+
+    }
+
+
+    public void btn4(ActionEvent event) {
+        XYChart.Series<String, Number> series3 = new XYChart.Series<String, Number>();
+        series3.getData().add(new XYChart.Data<String, Number>("Jan", 200));
+        series3.getData().add(new XYChart.Data<String, Number>("Feb", 600));
+        series3.getData().add(new XYChart.Data<String, Number>("Mar", 400));
+        series3.getData().add(new XYChart.Data<String, Number>("Apr", 400));
+        series3.getData().add(new XYChart.Data<String, Number>("May", 400));
+        series3.getData().add(new XYChart.Data<String, Number>("Jun", 400));
+        series3.getData().add(new XYChart.Data<String, Number>("Jul", 400));
+        series3.getData().add(new XYChart.Data<String, Number>("Aug", 400));
+        series3.getData().add(new XYChart.Data<String, Number>("Sep", 400));
+        series3.getData().add(new XYChart.Data<String, Number>("Okt", 400));
+        series3.getData().add(new XYChart.Data<String, Number>("Nov", 400));
+        series3.getData().add(new XYChart.Data<String, Number>("Dec", 400));
+
+        lineChart.getData().add(series3);
+    }
+
+
+    public void btn5(ActionEvent event) {
+        ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                        new PieChart.Data("Ochtend", 40),
+                        new PieChart.Data("Middag", 30),
+                        new PieChart.Data("Avond", 115));
+
+        pieChart.setData(pieChartData);
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
