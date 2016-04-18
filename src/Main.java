5a73a4e4e26c68a3a0cff26e95dc3a123af699ff
Kyrill van Seventer <k.vanseventer@gmail.com>
2016-04-18 11:30:09 +0200

    Code opgeschoond

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,11 @@
-import DBConnect.DBConnect;
-import DBConnect.Gui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import javax.swing.*;
-
 import java.sql.SQLException;
 
-import static javafx.application.Application.launch;
-
-
-//public class Main extends Application{
 public class Main extends Application{
-//    public static int Aantal_plaatsen = 0;
-
-    //Button button;
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -27,30 +15,11 @@ public class Main extends Application{
     }
 
 public static void main(String[] args) throws SQLException, ClassNotFoundException {
-//    Query query = new Query();
-//    query.queries();
-
-//    DBConnect dbConnect = new DBConnect();
-//    dbConnect.DBConnect();
 
     Query wijken = new Query();
     wijken.wijken();
 
     launch(args);
-//    Aantal_plaatsen +=1;
-//    Gui go = new Gui();
-//    go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//    go.setSize(300, 200);
-//    go.setVisible(true);
-
-
-//    if (Aantal_plaatsen == 40) ;
-//    {
-        //launch(args);
-//        go = new Gui();
-//        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        go.setSize(800, 200);
-//        go.setVisible(true);
     }
 
 
