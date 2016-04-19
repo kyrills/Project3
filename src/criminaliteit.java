2fbcf09cc49db75ccbf911d7abc1985db148f7db
Kyrill van Seventer <k.vanseventer@gmail.com>
2016-04-19 15:34:09 +0200

    interface en diefstallen aangepast

---
+++ src/criminaliteit.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Kyrilll van Seventer on 19/04/2016.
+ */
+public interface criminaliteit {
+
+    public String getArea();
+    public void setArea(String area);
+    public int getAantal();
+    public void setAantal(int aantal);
+    public int getWapenovervallen();
+
+}
