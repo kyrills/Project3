5aac7fdc7bfdeb65084d7d42f47b27334d088699
Kyrill van Seventer <k.vanseventer@gmail.com>
2016-04-20 12:52:21 +0200

    Code opgeschoond

--- src/Question1.java
+++ src/Question1.java
@@ -1,11 +1,5 @@
 import DBConnect.DBConnect;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Created by Kyrilll van Seventer on 14/04/2016.
- */
 
 public class Question1 {
     private ResultSet rs;
@@ -18,25 +12,6 @@ public class Question1 {
     public void queries() throws SQLException {
     }
 
-//    public List<diefstal> getFietsroven() throws SQLException {
-////        System.out.println(con);
-//        fietsroof =  "SELECT DISTINCT area, COUNT(area) AS Aantal FROM Neighbourhood_fietsroof WHERE location = \"ROTTERDAM\" GROUP BY area HAVING count(area) > 340";
-//        rs = con.executeQuery(fietsroof);
-//
-//        List<diefstal> fietsroofTotaals = new ArrayList<>();
-//        while (!rs.isLast()) {
-//            rs.next();
-//            diefstal fietsroofTotaal = new diefstal( rs.getString("area"), rs.getInt("Aantal"));
-//
-//
-////            System.out.println(fietsroofTotaal);
-//            fietsroofTotaals.add(fietsroofTotaal);
-//            Amount += 1;
-//        }
-//
-//        return fietsroofTotaals;
-//    }
-
     public ResultSet getFietsroven_1() throws SQLException {
         System.out.println(con);
         fietsroof =  "SELECT DISTINCT area, COUNT(area) AS Aantal FROM Neighbourhood_fietsroof WHERE location = \"ROTTERDAM\" GROUP BY area HAVING count(area) > 340";
@@ -44,41 +19,6 @@ public class Question1 {
 
         return rs;
     }
-
-//    public List<diefstal> getStraatroven() throws SQLException {
-//        System.out.println(con);
-//        straatroof = "SELECT DISTINCT area, COUNT(area) AS Aantal FROM Neighbourhood_straatroof WHERE location = \"ROTTERDAM\" GROUP BY area HAVING count(area) > 75";
-//        rs = con.executeQuery(straatroof);
-//
-//
-//        List<diefstal> straatroofTotaals = new ArrayList<>();
-//        while (!rs.isLast()) {
-//            rs.next();
-//            diefstal straatroofTotaal = new diefstal(rs.getString("area"), rs.getInt("Aantal"));
-//
-//            straatroofTotaals.add(straatroofTotaal);
-//            Amount += 1;
-//            System.out.println(straatroofTotaal);
-//        }
-//
-//        return straatroofTotaals;
-//    }
-//    public List<namen> getnamen() throws SQLException {
-//        namen = "SELECT DISTINCT area, COUNT(area) AS Aantal FROM Neighbourhood_straatroof WHERE location = \"ROTTERDAM\" GROUP BY area HAVING count(area) > 75";
-//        rs = con.executeQuery(namen);
-//
-//        List<namen> areas = new ArrayList<>();
-//        while (!rs.isLast()) {
-//            rs.next();
-//            namen area1 = new namen (rs.getString("area"));
-//
-//            areas.add(area1);
-//            Amount += 1;
-//        }
-//
-//    }
-
-
 }
 
 
--- src/Question5.java
+++ src/Question5.java
@@ -1,44 +1,13 @@
 import DBConnect.DBConnect;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Created by Samuel on 18-04-16.
- */
-//" "
-
 
 public class Question5 {
     private ResultSet rs;
     public int Amount = 0;
     public String fietsmerk;
     DBConnect con = new DBConnect();
-//    public void queries() throws SQLException {
-//    }
 
-//    public List<Answer5> getFiestMerk() throws SQLException {
-//
-//        System.out.println(con);
-//
-//        fietsmerk = "SELECT fr.brand_type, count(fr.object) FROM Fietsroof fr WHERE object = 'FIETS' GROUP BY fr.brand_type HAVING count(fr.object) > 150";
-////        System.out.println(fietsmerk);
-//        rs = con.executeQuery(fietsmerk);
-//
-//        List<Answer5> BuitTotaals = new ArrayList<>();
-//        while (!rs.isLast()) {
-//            rs.next();
-//            Answer5 buitTotaal = new Answer5( rs.getString("fr.brand_type"), rs.getInt("count(fr.object)"));
-////            System.out.println(buitTotaal);
-//
-//            BuitTotaals.add(buitTotaal);
-//            Amount += 1;
-//        }
-//
-//        return BuitTotaals;
-//    }
     public ResultSet getBuitTotaals_1() throws SQLException {
         fietsmerk = "SELECT fr.brand_type, count(fr.object) FROM Fietsroof fr WHERE object = 'FIETS' AND brand_type <> 'ONBEKEND' GROUP BY fr.brand_type HAVING count(fr.object) > 150";
         rs = con.executeQuery(fietsmerk);
